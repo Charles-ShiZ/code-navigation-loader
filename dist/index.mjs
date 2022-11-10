7770b0a3f42e1573b4efd483dff141f0b8479997
shizhanhong <Charles.ShiZ@outlook.com>
2022-11-10 12:14:46 +0800

    update

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,44 +1,37 @@
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __commonJS = (cb, mod) => function __require() {
-  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
-};
-
 // src/index.ts
-var require_src = __commonJS({
-  "src/index.ts"(exports, module) {
-    function codeNavigationLoader(content) {
-      const options = this.getOptions();
-      const filePath = this.resourcePath;
-      const componentNames = options.componentNames;
-      const contentSplit = content.split(/\n/).map((str) => `${str}
+function codeNavigationLoader(content) {
+  const options = this.getOptions();
+  const filePath = this.resourcePath;
+  const componentNames = options.componentNames;
+  const contentSplit = content.split(/\n/).map((str) => `${str}
 `);
-      const matchedPositions = [];
-      const compNames = componentNames.map((name, index) => `(${name})${index + 1 !== componentNames.length ? "|" : ""}`).join("");
-      const reactCompRegExp = new RegExp(`<(${compNames})+(?=(\\s|>))`, "g");
-      contentSplit.forEach((eachLine, line) => {
-        const char = eachLine.search(reactCompRegExp);
-        if (char > -1) {
-          matchedPositions.push({
-            line: line + 1,
-            char: char + 1
-          });
-        }
+  const matchedPositions = [];
+  const compNames = componentNames.map((name, index) => `(${name})${index + 1 !== componentNames.length ? "|" : ""}`).join("");
+  const reactCompRegExp = new RegExp(`<(${compNames})+(?=(\\s|>))`, "g");
+  contentSplit.forEach((eachLine, line) => {
+    const char = eachLine.search(reactCompRegExp);
+    if (char > -1) {
+      matchedPositions.push({
+        line: line + 1,
+        char: char + 1
       });
-      let i = 0;
-      return content.replace(reactCompRegExp, (matchString) => {
-        const { line, char } = matchedPositions[i];
-        const subString = `${matchString} 
+    }
+  });
+  let i = 0;
+  return content.replace(reactCompRegExp, (matchString) => {
+    const { line, char } = matchedPositions[i];
+    const subString = `${matchString} 
         onContextMenu= {(e) => {
             if(e.shiftKey){
                 e.preventDefault();
+                e.stopPropagation();
                 window.open('vscode://file/${filePath}:${line}:${char}')
             }
         }}`;
-        i++;
-        return subString;
-      });
-    }
-    module.exports = codeNavigationLoader;
-  }
-});
-export default require_src();
+    i++;
+    return subString;
+  });
+}
+export {
+  codeNavigationLoader as default
+};
